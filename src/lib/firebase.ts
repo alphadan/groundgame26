8ef29bff91a148479ad1c88b065cc8ee1ca74221
alphadan <info@alphabetsigns.com>
2025-11-26 17:00:46 -0500

    SECURITY: Move Firebase config to .env.local – no more exposed keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRfOLJKZNkGdMrgXNFFCgybGpDS--kl5g",
-  authDomain: "groundgame26.firebaseapp.com",
-  databaseURL: "https://groundgame26-default-rtdb.firebaseio.com",
-  projectId: "groundgame26",
-  storageBucket: "groundgame26.firebasestorage.app",
-  messagingSenderId: "707878592124",
-  appId: "1:707878592124:web:8566758659b79e519773f9",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
